4943d2fe45bf8fe480af4da16503add237123d60
abdallah-abdelnaby <142503679+abdallah-abdelnaby@users.noreply.github.com>
2024-05-30 01:08:46 +0200

    Create upload_to_ola_hd.sh

---
+++ upload_to_ola_hd.sh
@@ -0,0 +1,19 @@
+#this script is done to upload ocr bagit to OLA-HD
+
+
+#ola-hd username:password
+OLA=$1
+
+#ocr bagit path
+OCRD_RESULTS=$2
+
+upload_to_ola_hd() {
+    echo "Uploading the results to OLA-HD..."
+    curl -X POST 141.5.99.53/api/bag -u "$OLA" -H 'content-type: multipart/form-data' -F file=@"$OCRD_RESULTS"
+    if [ $? -ne 0 ]; then
+        log_error "Failed to download the results."
+        exit 1
+    fi
+}
+
+upload_to_ola_hd
